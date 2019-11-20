cf18f2d08a2d4004a964709bf9a3eff0f61f50c6
colaalex <colaalex4r@mail.ru>
2019-11-20 23:36:49 +0900

    task2 attempt2

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
+sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
+# i'm not sure how to do it correctly but now i launched with gunicorn -c etc/hello.py hello:wsgi_application
+# sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+# sudo /etc/init.d/gunicorn restart 
 #sudo /etc/init.d/mysql start
