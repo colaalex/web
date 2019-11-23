642624fd0cacd84415cf799f919996bb3bcf8379
colaalex <colaalex4r@mail.ru>
2019-11-23 17:55:16 +0900

    task4 final

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-# i dunno how it starts some troubles
-. env/bin/activate
+sudo pip3 install django==2.0.5
+sudo pip3 install gunicorn
+sudo pip3 install mysqlclient==1.3.12
 sudo gunicorn -c /home/box/web/etc/hello.py hello:wsgi_application &
 sudo gunicorn -c /home/box/web/etc/gunicorn-django.py ask.wsgi:application &
